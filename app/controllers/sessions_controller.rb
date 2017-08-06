d3a8cd9f24e88574184dcd4ad2ec76e449f569e9
airi.iwama <airxxxx1209@gmail.com>
2017-08-06 04:13:31 +0000

    post

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,6 +20,9 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:success] = 'ログアウトしました。'
+    redirect_to root_url
   end
 
   private
@@ -35,5 +38,4 @@ class SessionsController < ApplicationController
       return false
     end
   end
-end
 end
